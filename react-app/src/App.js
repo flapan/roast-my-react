d09977649d78625bb2a8ce3c28531d4610ce5bce
flapan <mikkel@gadegaard.eu>
2023-01-12 17:08:37 -0800

    testing useReducer hook

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,27 +1,14 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useReducer } from 'react';
 
 function App() {
-  const [emotion, setEmotion] = useState("happy");
-  const [secondaryEmotion, setSecondaryEmotion] = useState("tired");
-
-  useEffect(() => {
-    console.log(`It's ${emotion} right now`)
-  }, [emotion]);
-
-  useEffect(() => {
-    console.log(`It's ${secondaryEmotion} right now`)
-  }, [secondaryEmotion]);
-
+  const [checked, setChecked] = useReducer((checked) => !checked, false);
   return (
     <div className="App">
-      <h1>Current emotion is {emotion}</h1>
-      <button onClick={() => setEmotion("sad")}>Sad</button>
-      <button onClick={() => setEmotion("happy")}>Happy</button>
-      <button onClick={() => setEmotion("excited")}>Excited</button>
-      <h2>Current Secondary emotion is {secondaryEmotion}.</h2>
-      <button onClick={() => setSecondaryEmotion("grateful")}>Grateful</button>
-      <button onClick={() => setSecondaryEmotion("refreshed")}>Refreshed</button>
+      <input type="checkbox" 
+      value={checked} 
+      onChange={setChecked} />
+      <label> {checked ? "Checked" : "Not Checked"}</label>
     </div>
   );
 }
